6e57440ee1b2751b50a794b96f16fa84d540a8d1
KaikeVitorino <Georgesilva10#@$>
2023-03-21 22:49:17 -0300

    Casa

---
+++ Seleção/9.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x float64
+	var y float64
+	var z float64
+		fmt.Print("Digite 3 numeros Reais: ")
+		fmt.Scan(&x, &y, &z)
+		if x<y && y<z {
+			fmt.Println("Os numeros em ordem crescente: ", x, y, z)
+		}else if z>y && z<x {
+			fmt.Println("Os numeros em ordem crescente: ", y, z, x)
+		}else if x>z && y<z {
+			fmt.Println("Os numeros em ordem crescente: ", x, z, y)
+		}else if x>z && x<y {
+			fmt.Println("Os numeros em ordem crescente: ", z, x, y)
+		}else if x>z && y<z {
+			fmt.Println("Os numeros em ordem crescente: ", z, y, x)
+		}
+}
