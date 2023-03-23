cfa16a75b22699ffe7221e8c094f36f6575d0fcf
KaikeVitorino <Georgesilva10#@$>
2023-03-23 15:02:29 -0300

    Casa

---
+++ Repetição/2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	numeros := []string{"2", "4", "6", "8", "10", "12", "14", "16", "18", "20"}
+
+	for index, num := range numeros {
+		fmt.Printf("Numero par %d: %s\n", index, num)
+	}
+}
